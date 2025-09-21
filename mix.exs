ca0f24abd1038063eee4e7717b8c01207f3d7ce2
andimon <andrevella2001@gmail.com>
2025-09-21 23:02:28 +0200

    fix: covearll

--- mix.exs
+++ mix.exs
@@ -10,7 +10,15 @@ defmodule TestingAutomaticElixirRelease.MixProject do
       deps: deps(),
       description: "A package that is used in the testing of an automatic Elixir pipeline",
       package: package(),
-      source_url: "https://github.com/andimon/testing-automatic-elixir-release"
+      source_url: "https://github.com/andimon/testing-automatic-elixir-release",
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test,
+        "coveralls.json": :test
+      ]
     ]
   end
 
@@ -26,7 +34,8 @@ defmodule TestingAutomaticElixirRelease.MixProject do
     [
       {:ex_doc, "~> 0.38.4", only: :dev, runtime: false},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false}
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.18", only: :test}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
